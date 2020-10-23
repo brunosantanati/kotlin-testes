8e877f90b5a0086f4e0b17b34b1df349d8c74de5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-23 10:28:17 -0300

    When example and regex example(11 or more digits).

--- src/me/bruno/santana/EnumExample.kt
+++ src/me/bruno/santana/EnumExample.kt
@@ -7,6 +7,15 @@ enum class DictTypeEnum(val type: Int){
     EVP(4)
 }
 
+fun getDictType(key: String): Int{
+    return when {
+        key.startsWith("+") -> DictTypeEnum.PHONE.type
+        key.contains("@") -> DictTypeEnum.EMAIL.type
+        key.matches("^\\d{11,}\$".toRegex()) -> DictTypeEnum.DOCUMENT.type //11 or more digits
+        else -> DictTypeEnum.EVP.type
+    }
+}
+
 fun main() {
 
     val document: DictTypeEnum = DictTypeEnum.DOCUMENT
@@ -18,4 +27,10 @@ fun main() {
     println(DictTypeEnum.EMAIL.type)
     println(DictTypeEnum.PHONE.type)
     println(DictTypeEnum.EVP.type)
+
+    println("############## Discover Dict Type ################")
+    println(getDictType("06341897000101"))
+    println(getDictType("bruno@bruno.com"))
+    println(getDictType("+5511991918538"))
+    println(getDictType("0a02d8e4-b108-45ca-b8b3-9dc6e29cfa2a"))
 }
