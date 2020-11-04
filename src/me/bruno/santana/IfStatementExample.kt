a70bb33780881240bfcdda66da9485ef965250af
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-11-04 12:11:43 -0300

    If Statement example.

--- src/me/bruno/santana/IfStatementExample.kt
+++ src/me/bruno/santana/IfStatementExample.kt
@@ -3,6 +3,8 @@ package me.bruno.santana
 fun main() {
     print("CPF? ")
     println(if(isCPF("78945612365")) "yes" else "no")
+    print("CNPJ? ")
+    println(if(!isCPF("78945612365145")) "yes" else "no")
 }
 
 fun isCPF(document: String) = document.length <= 11
