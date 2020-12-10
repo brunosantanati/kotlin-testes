ad3b798ae1bce53ab0cd017f44e6f747cdd7accf
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-10 19:35:43 -0300

    Filter list by another one(get all differents).

--- kotlin-testes-gradle/src/main/kotlin/me/brunosantana/FilterAListByAnotherOne.kt
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/FilterAListByAnotherOne.kt
@@ -9,7 +9,8 @@ fun main() {
         BankSlip("02145569874523654122220000147896520147852000"),
         BankSlip("02145569874523654122220000147896520147852001"),
         BankSlip("02145569874523654122220000147896520147852002"),
-        BankSlip("02145569874523654122220000147896520147852003")
+        BankSlip("02145569874523654122220000147896520147852003"),
+        BankSlip("02145569874523654122220000147896520147852004")
     )
 
     val depositBankSlips = listOf<BankSlip>(
@@ -17,14 +18,23 @@ fun main() {
         BankSlip("02145569874523654122220000147896520147852001")
     )
 
+    //maintain equals
     val filteredList = allBankSlips.filterByBarCode(depositBankSlips)
     filteredList.forEach{ println(it) }
 
     println("-----------------------------------")
 
     //the same logic/code but without using a separated function
+    //maintain equals
     allBankSlips.filter {
         bankSlip -> depositBankSlips.any { it.barCode == bankSlip.barCode }
+    }.forEach{ println(it) }
+
+    println("-----------------------------------")
+
+    //get all differents
+    allBankSlips.filter {
+        bankSlip -> depositBankSlips.all { it.barCode != bankSlip.barCode }
     }.forEach{ println(it) }
 }
 
