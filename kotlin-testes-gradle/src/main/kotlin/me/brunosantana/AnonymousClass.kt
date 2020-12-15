66cbf1d4076d4ea92d69ff8f3daf3f7fd8f829d5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-14 23:36:29 -0300

    Use Kotlin 1.4 and lambda instead of anonymous class.

--- kotlin-testes-gradle/src/main/kotlin/me/brunosantana/AnonymousClass.kt
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/AnonymousClass.kt
@@ -1,12 +1,22 @@
 package me.brunosantana
 
 //doc: https://kotlinlang.org/docs/reference/object-declarations.html
+//https://stackoverflow.com/questions/43737785/kotlin-interface-does-not-have-constructors
+//https://kotlinlang.org/docs/reference/whatsnew14.html#sam-conversions-for-kotlin-interfaces
 
-interface Animal{
+// From now on, you can use SAM conversions for Kotlin interfaces as well.
+// To do so, mark a Kotlin interface explicitly as functional with the fun modifier.
+fun interface Animal{
     fun eat()
 }
 
+fun interface Flyable{
+    fun fly(flyLongDistances: Boolean)
+}
+
 fun main() {
+
+    //anonymous classes
     val horse = object: Animal{
         override fun eat() {
             println("Horse eating...")
@@ -19,7 +29,28 @@ fun main() {
         }
     }
 
+    //lambda (using functional interface with method without parameter)
+    val dog = Animal { println("Dog eating...") }
+
     horse.eat()
     cow.eat()
+    dog.eat()
+
+    //#####################################################
+
+    //anonymous class
+    val dove = object: Flyable{
+        override fun fly(flyLongDistances: Boolean) {
+            println("I'm flying... fly long distances? $flyLongDistances")
+        }
+
+    }
+
+    //lambda (using functional interface with method with parameter)
+    val dove2 = Flyable { flyLongDistances -> println("I'm flying... fly long distances? $flyLongDistances") }
+
+    dove.fly(true)
+    dove2.fly(true)
+
 }
 
