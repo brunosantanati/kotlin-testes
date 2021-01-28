a3162f0d7b5ecc817cd6502b0db94ea6e2f03815
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-28 12:18:46 -0300

    Example using generics and interface with T.

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/GenericsExample.kt
@@ -0,0 +1,35 @@
+package me.brunosantana
+
+interface DataLakeUpdater<T>{
+    fun updateDataLake(data: String): List<T>
+}
+
+data class Credit(val value: String)
+
+data class Account(val value: String)
+
+class CreditManualUpdater: DataLakeUpdater<Credit>{
+    override fun updateDataLake(data: String): List<Credit> {
+        val list = mutableListOf<Credit>()
+        for (i in 0..9){
+            list.add(Credit("$data$i"))
+        }
+        return list
+    }
+}
+
+class AccountManualUpdater: DataLakeUpdater<Account>{
+    override fun updateDataLake(data: String): List<Account> {
+        val list = mutableListOf<Account>()
+        for (i in 0..9){
+            list.add(Account("$data$i"))
+        }
+        return list
+    }
+
+}
+
+fun main() {
+    println(CreditManualUpdater().updateDataLake("credit"))
+    println(AccountManualUpdater().updateDataLake("account"))
+}
