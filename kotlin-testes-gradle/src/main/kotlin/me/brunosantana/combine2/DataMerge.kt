7318055451282e518b05d854937949e7c90355a5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-03-04 16:57:50 -0300

    Recursive function to print deeply a map.

--- kotlin-testes-gradle/src/main/kotlin/me/brunosantana/combine2/DataMerge.kt
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/combine2/DataMerge.kt
@@ -6,9 +6,27 @@ class DataMerge {
      */
     fun jsonMerge(json1: String, json2: String): String?{
         val map1 = jsonStringToMap(json1)
+        printMap(map1); println()
+
         val map2 = jsonStringToMap(json2)
+        printMap(map2); println()
+
         val mapMerge = (map1 - map2)+map2
+        printMap(mapMerge); println()
+
         return mapToJsonString(mapMerge);
+    }
+
+    private fun printMap(map: Map<*, *>){
+        for ((k, v) in map) {
+            print("$k")
+            if(v is Map<*, *>){
+                print(" -> ")
+                printMap(v)
+            }else{
+                println(" = $v")
+            }
+        }
     }
 }
 
