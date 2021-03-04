7efd0426ea0d261a4aa914f58638c7ae5d04a41c
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-03-04 16:34:57 -0300

    Print array deeply.

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/PrintArrayDeeply.kt
@@ -0,0 +1,11 @@
+﻿package me.brunosantana
+
+fun main() {
+    val matrix = arrayOf(
+        intArrayOf(3, 7, 9),
+        intArrayOf(0, 1, 0),
+        intArrayOf(2, 4, 8)
+    )
+
+    println(matrix.contentDeepToString()) // [[3, 7, 9], [0, 1, 0], [2, 4, 8]]
+}
