23a977509c306add82bcc88513936f697eb9799c
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-03-19 13:58:30 -0300

    Nullable example.

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/NullableExample.kt
@@ -0,0 +1,12 @@
+﻿package me.brunosantana
+
+data class MyClassWithNullableField(
+    val myField: String,
+    var myNullableField: String? = null,
+)
+
+fun main() {
+    val myObj = MyClassWithNullableField("field1")
+    myObj.myNullableField = "field2"
+    println(myObj)
+}
