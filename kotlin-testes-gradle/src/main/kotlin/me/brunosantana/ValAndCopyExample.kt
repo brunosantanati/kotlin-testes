dd39c7bc37d67dc59d16aa2f76122131ddde53d3
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-04-01 14:21:21 -0300

    Example copying a obj with only val fields.

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/ValAndCopyExample.kt
@@ -0,0 +1,22 @@
+﻿package me.brunosantana
+
+data class ValAndCopyExample(
+    val field1: String,
+    val field2: String,
+    val field3: String,
+)
+
+fun main() {
+    val obj1 = ValAndCopyExample(
+        field1 = "campo 1",
+        field2 = "campo 2",
+        field3 = "campo 3"
+    )
+
+    val objCopy = obj1.copy(
+        field1 = "field 1"
+    )
+
+    println(obj1)
+    println(objCopy)
+}
