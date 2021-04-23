7afa2c232237e05234eeae01bd0fe7ec77ec318f
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-04-23 14:04:39 -0300

    Example sending CSV as InputStream to S3.

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/S3Example2.kt
@@ -0,0 +1,59 @@
+﻿package me.brunosantana
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import java.util.ArrayList
+import com.opencsv.CSVWriter
+import com.opencsv.bean.StatefulBeanToCsvBuilder
+import com.opencsv.bean.StatefulBeanToCsv
+import java.io.*
+
+
+//https://www.baeldung.com/opencsv
+//https://docs.aws.amazon.com/pt_br/sdk-for-java/v1/developer-guide/examples-s3-objects.html#upload-object
+//https://docs.aws.amazon.com/pt_br/sdk-for-java/v1/developer-guide/setup-credentials.html
+
+/*Antes de rodar criar os arquivos:
+
+C:\Users\55119\.aws\config
+[default]
+region = us-east-2
+
+C:\Users\55119\.aws\credentials
+[default]
+aws_access_key_id = [put_access_key_here]
+aws_secret_access_key = [put_secret_key_here]
+
+*/
+
+data class MyBean2(
+    val colA: String,
+    val colB: String,
+    val colC: String,
+)
+
+fun main() {
+
+    val s3: AmazonS3 = AmazonS3ClientBuilder.defaultClient()
+    s3.putObject("bruno-csv-bucket", "my-test-2.csv", getCsvAsInputStream(), null);
+
+}
+
+fun getCsvAsInputStream(): InputStream {
+    val stream = ByteArrayOutputStream()
+    val streamWriter = OutputStreamWriter(stream)
+    val writer = CSVWriter(streamWriter)
+
+    val beanToCsv: StatefulBeanToCsv<MyBean2> = StatefulBeanToCsvBuilder<MyBean2>(writer)
+        .withSeparator(';') //It's not working in this example, the csv is being create with "," as delimiter
+        .build()
+
+    val list: MutableList<MyBean2> = ArrayList<MyBean2>()
+    list.add(MyBean2("Linha 1 Coluna A", "Linha 1 Coluna B", "Linha 1 Coluna C"))
+    list.add(MyBean2("Linha 2 Coluna A", "Linha 2 Coluna B", "Linha 2 Coluna C"))
+
+    beanToCsv.write(list)
+    streamWriter.flush()
+
+    return ByteArrayInputStream(stream.toByteArray());
+}
