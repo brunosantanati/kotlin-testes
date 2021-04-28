c45f54fd32cb1ebe10698eda3545c6ea849f7471
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-04-28 11:29:11 -0300

    Example how to join two maps(sum them).

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/JoinMaps.kt
@@ -0,0 +1,25 @@
+﻿package me.brunosantana
+
+fun main() {
+    val mapCommon = mapOf(
+        "chave1" to "valor1",
+        "chave2" to "valor2",
+        "chave3" to "valor3"
+    )
+
+    val map1 = mapOf(
+        "chave4" to "valor4",
+        "chave5" to "valor5"
+    )
+
+    val map2 = mapOf(
+        "chave10" to "valor10",
+        "chave20" to "valor20"
+    )
+
+    val map3 = mapCommon + map1
+    val map4 = mapCommon.plus(map2)
+
+    println(map3)
+    println(map4)
+}
