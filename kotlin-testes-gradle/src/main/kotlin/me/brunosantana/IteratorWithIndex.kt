9323e7954ddd0859e603f941a4568f250b4401ea
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-05-03 14:19:16 -0300

    Example using Iterator.withIndex().

---
+++ kotlin-testes-gradle/src/main/kotlin/me/brunosantana/IteratorWithIndex.kt
@@ -0,0 +1,16 @@
+﻿package me.brunosantana
+
+fun main() {
+    val list = listOf("Thor", "Hulk", "Spider-man")
+    val iterator = list.iterator()
+    for((index, item) in iterator.withIndex()){
+        println("$index $item")
+    }
+
+    println()
+
+    val iterator2 = list.iterator()
+    for((index, item) in iterator2.withIndex()){
+        println("${index + 1} $item")
+    }
+}
